b9c311d650931e30ec1a58366ea69d7b87b9fcd4
RohitGadhave <>
2022-04-26 11:08:56 +0000

    array function

--- Array/array.js
+++ Array/array.js
@@ -1,3 +1,30 @@
 console.log('Hi i am array');
+// reduce
+
+
+
+
+
+
+
+
+
 
 let arrayOfNumber = [];
+
+
+
+
+
+
+
+//############################################
+const arrayOfOddNumbers = [1, 3, 5];
+arrayOfOddNumbers[100] = 199;
+console.log("Array length on adding 100th index value");
+console.log(arrayOfOddNumbers.length);//101
+
+/*
+The reason for this solution is as follows: JavaScript placesempty as a value for indices 3 - 99. Thus, when you set the value of the 100th index, the array looks like:
+(101) [1, 3, 5, empty × 97, 199]
+*/
--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 console.log('Om Sai Ram');
 
-// require('./Array/array');
+require('./Array/array');
 // require('./Loops/loops');
 // require('./Common/Assignment_Destructuring');
 // require('./Common/SpreadOperatorAndRestParameters');
-require('./Functions/functions');
+// require('./Functions/functions');
+
+
+let arrayOfLetters = ['a','b','c','d','e','f'];
+const anotherArrayOfLetters = arrayOfLetters;
+arrayOfLetters = [];
+console.log(anotherArrayOfLetters,arrayOfLetters);//['a','b','c','d','e','f']
+
+/*
+When we carry out the above (containing arrayOfLetters = []) you create a new array and break the old reference. Thus, anotherArrayOfLetters has no effect on it since it is still pointing to the old array.
+*/
+
+
+// How could you print unique values from an array?
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+function getUniqueValues(arrOfNum){
+  // Way 1
+  // const set = new Set(arrOfNum);
+  // return [...set];
+  // Way 2
+  let iNo=0;lNo = arrOfNum.length,tempArray = [];
+    let flag = false;
+  for(iNo;iNo<lNo;iNo++){
+    let jNo=0;
+    let tempValue = arrOfNum[iNo];
+    console.log("tempValue",iNo,jNo,lNo)
+    flag = true;
+    for(jNo;jNo<lNo;jNo++){
+      console.log(arrOfNum[jNo],jNo)
+      if(jNo==iNo){
+        // continue
+        
+      }
+      else if(tempValue== arrOfNum[jNo]) {
+        flag = false
+      };
+    }
+    if(flag) tempArray.push(arrOfNum[iNo]);
+    
+  }
+  return tempArray;
+}
+console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+
